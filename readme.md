b702a3aa54e66d0122aa414d169ea78b845b2df5
Boris Strahija <bstrahija@gmail.com>
2021-09-22 14:37:08 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# OptimizePress UI
+
+...
---
+++ webpack.mix.js
@@ -0,0 +1,15 @@
+let mix = require('laravel-mix');
+
+// Define the public and resources paths
+mix.setPublicPath('dist');
+// mix.setResourceRoot('../');
+
+// Compile assets
+mix.sass('scss/ui.scss', 'dist/css/ui.css')
+    .sourceMaps();
+
+mix.sass('scss/ui-reboot.scss', 'dist/css/reboot.css')
+    .sourceMaps();
+
+mix.sass('scss/ui-grid.scss', 'dist/css/grid.css')
+    .sourceMaps();
