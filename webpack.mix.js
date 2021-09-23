e3c1028cd795bd5ce0de683c8f33ab1957b0157c
Boris Strahija <bstrahija@gmail.com>
2021-09-23 16:28:08 +0200

    Refactor structure, better test templates

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,26 @@
-let mix = require('laravel-mix');
+let mix  = require('laravel-mix');
+const del = require('del');
+require('laravel-mix-ejs');
 
 // Define the public and resources paths
 mix.setPublicPath('dist');
 mix.setResourceRoot('../');
 
 // Compile assets
-mix.sass('scss/ui.scss', 'dist/css/ui.css')
+mix.sass('src/scss/ui.scss', 'dist/css/ui.css')
     .sourceMaps();
 
-mix.sass('scss/ui-reboot.scss', 'dist/css/reboot.css')
+mix.sass('src/scss/ui-reboot.scss', 'dist/css/reboot.css')
     .sourceMaps();
 
-mix.sass('scss/ui-grid.scss', 'dist/css/grid.css')
+mix.sass('src/scss/ui-grid.scss', 'dist/css/grid.css')
     .sourceMaps();
 
 // Copy fonts
-mix.copyDirectory('fonts', 'dist/fonts');
+mix.copyDirectory('src/fonts', 'dist/fonts');
+
+// Test UI
+mix.copy('src/tests/tests.css', 'dist/tests');
+mix.copy('src/tests/tests.js', 'dist/tests');
+mix.ejs('src/tests/index.ejs', 'dist/tests');
+mix.ejs('src/tests/bootstrap.ejs', 'dist/tests');
