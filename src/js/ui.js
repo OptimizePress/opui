2dbee218e00cde14a5838f5a4d4cbf26be7768f9
Boris Strahija <bstrahija@gmail.com>
2021-10-01 16:38:45 +0200

    Dist

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,5 +1,6 @@
-import {toggleFullscreen} from './fullscreen'
+import { toggleFullscreen } from './fullscreen'
+import { checkSwitch } from './check-switch'
 
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Loaded OPUI scripts", toggleFullscreen);
+    console.log("Loaded OPUI scripts", toggleFullscreen, checkSwitch);
 });
