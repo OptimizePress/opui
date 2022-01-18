c9ae7440fa6c1307d3dfdaa8b1bac862ce677742
rlabrovi <roko.labrovic@gmail.com>
2022-01-18 14:27:14 +0100

    Removing check-switch because we aren't using in in any other proect and from now on we should use toggle.scss

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,7 +1,5 @@
 import { toggleFullscreen } from "./fullscreen";
-import { checkSwitch } from "./check-switch";
-import Toggle from "./components/toggle";
 
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Loaded OPUI scripts", toggleFullscreen, checkSwitch, Toggle);
+    console.log("Loaded OPUI scripts", toggleFullscreen);
 });
